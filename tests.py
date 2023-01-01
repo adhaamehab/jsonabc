4c61c3cb76bb1f6811a2e6de9cfcb3d90a208de7
Adham Ehab <adham.ehab@cision.com>
2023-01-01 14:02:15 +0200

    Add base for running tests
    Signed-off-by: Adham Ehab <adham.ehab@cision.com>

---
+++ tests.py
@@ -0,0 +1,18 @@
+
+from jsonabc import JSONABC
+
+
+class Person(metaclass=JSONABC):
+    name: str
+    age: int
+    address: str
+
+
+def test_case_1():
+    data = {"name": "John", "age": 30, "address": "123 Main St"}
+    person = Person(data)
+    assert person.name == "John"
+    assert person.age == 30
+    assert person.address == "123 Main St"
+    assert person.json() == data
+
